682a56d7b6f46ca0a0d16dbdfcbe4f70b486042b
wbh <showmana@naver.com>
2020-10-28 22:55:42 +0900

    MyPractice.Book, MyPractice.Student, MyPractice.StudentTest

---
+++ MyPractice/src/chapter6/StudentTest.java
@@ -0,0 +1,23 @@
+package chapter6;
+
+public class StudentTest {
+
+	public static void main(String[] args) {
+		
+		
+
+	}
+
+}
+
+//	문제
+// 어느 학교에 학생이 3명 있습니다. 각 학생마다 읽은 책을 기록하고 있습니다.
+// Student 클래스를 만들고 각 학생마다 읽은 책을 Student 클래스 내에 ArrayList를 생성하여 관리하도록 합니다.
+// 다음과 같이 출력되도록 Student, Book, StudentTest 클래스를 만들어 실행하세요.
+
+
+
+//  출력되어야할 값
+//	Lee 학생이 읽은 책은 : 태백산맥1 태백산맥2 입니다.
+//	Kim 학생이 읽은 책은 : 토지1 토지2 토지3 입니다.
+//	Cho 학생이 읽은 책은 : 해리포터1 해리포터2 해리포터3 해리포터4 해리포터5 해리포터6 입니다.
--- Practice/src/chapter6/Student.java
+++ Practice/src/chapter6/Student.java
@@ -7,7 +7,7 @@ public class Student {
 	int studentID;
 	String studentName;
 	ArrayList<Book> bookList;
-	int index;
+//	int index;
 	
 	public Student(int studentID, String studentName){
 		this.studentID = studentID;
